213fc0957410fed2fa550d7bcdb7b74cb8dc5ced
Tslat <tslatmail@gmail.com>
2024-05-19 21:45:31 +1000

    Fix javadoc

--- Common/src/main/java/net/tslat/effectslib/api/util/MemoizedFunction.java
+++ Common/src/main/java/net/tslat/effectslib/api/util/MemoizedFunction.java
@@ -7,7 +7,7 @@ import java.util.function.Function;
 
 /**
  * Memoizing function that caches the result of computed values to save additional lookup on future calls.
- * <p>Similar to {@link net.minecraft.Util.memoize() Util#memoize}, but utilises FastUtil instead of a concurrent map for a faster implementation</p>
+ * <p>Similar to {@link net.minecraft.Util#memoize Util.memoize}, but utilises FastUtil instead of a concurrent map for a faster implementation</p>
  */
 public class MemoizedFunction<I, O> implements Function<I, O> {
     private final Map<I, O> cache = new Object2ObjectOpenHashMap<>();
